584a0ae6c04534cf5c374c9ba043cb251d23a8a8
Joel <joel2006sm@gmail.com>
2025-11-04 17:51:44 +0100

    act

--- Empleado.php
+++ Empleado.php
@@ -7,7 +7,7 @@ class Empleado{
     public $salario_base;
 
     //Constructor
-    public function __constructor($nombre,$apellido,$salario_base){
+    public function __construct($nombre,$apellido,$salario_base){
         $this->nombre = $nombre;
         $this->apellidos = $apellidos;
         $this->salario_base = $salario_base;
@@ -30,12 +30,15 @@ class Gerente extends Empleado{
     public $bono;
 
     //Constructor
-    public function __constructor($nombre,$apellidos,$salario_base){
+    public function __construct($nombre,$apellidos,$salario_base){
          parent::__construct($nombre,$apellidos,$salario_base);
+         $this->bono = $salario_base * 0.10;
     }
-
-    $this->bono = $salario_base * 10 / 100;
-    $salarioAnual = $this->salario_base + $this->bono;
+     public function calcularSalarioAnual() {
+        $salarioAnual = ($this->salario_base + $this->bono) * 12;
+        return "Salario anual de " . $this->devolverNombreCompleto() . "es $salarioAnual €";
+    }
+    
 }
 
 class Vendedor extends Empleado{
@@ -44,12 +47,15 @@ class Vendedor extends Empleado{
     public $comision;
 
     //Constructor
-    public function __constructor($nombre,$apellidos,$salario_base){
+    public function __construct($nombre,$apellidos,$salario_base){
          parent::__construct($nombre,$apellidos,$salario_base);
+         $this->comision = $salario_base * round(0,10) / 100;
     }
 
-    $this->comision = $salario_base * round(0,10) / 100;
-    $salarioAnual = $this->salario_base + $this->comision;
+    public function calcularSalarioAnual() {
+        $salarioAnual = ($this->salario_base + $this->comision) * 12;
+        return "Salario anual de" . $this->devolverNombreCompleto() . "es $salarioAnual €";
+    }
 }
 
 ?>
